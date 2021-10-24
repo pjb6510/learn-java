cbaf57840fb2c9911de976bee5c1b102be93a1e1
ParkJeongbeen <pjb6510@gmail.com>
2021-10-24 16:05:37 +0900

    fix: StaticMember main function

--- src/10.Static/StaticMember.java
+++ src/10.Static/StaticMember.java
@@ -2,7 +2,7 @@ public class StaticMember {
   int number = 10;
   static int staticNumber = 10;
 
-  public static void main() {
+  public static void main(String[] args) {
     // System.out.println(this.number);
     System.out.println(StaticMember.staticNumber);
   }
